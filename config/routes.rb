15927610ce77fdb16ad7a7efcf37dda030a175a9
Pablo Barrios <ingpablobarrios@gmail.com>
2015-06-01 20:06:27 -0300

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Testplupload::Application.routes.draw do
   resources :presses
 
-  match "/" => 'presses#index'
   root :to  => 'presses#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,26 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20150601182349) do
 
-  create_table "announcements", :force => true do |t|
-    t.string   "title"
-    t.string   "description"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
-  create_table "press_mentions", :force => true do |t|
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
   create_table "presses", :force => true do |t|
     t.string   "title"
     t.string   "description"
